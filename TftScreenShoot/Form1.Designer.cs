672ed741e51de4976a9792e98ad13d18c5f414bd
torpil2 <bugraksener@gmail.com>
2021-12-18 06:47:18 +0300

    Add project files.

---
+++ TftScreenShoot/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+
+namespace TftScreenShoot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TftScreenShoot/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+
+using Utilities;
+
+namespace TftScreenShoot
+{
+
+
+    public partial class Form1 : Form
+    {
+        globalKeyboardHook gkh = new globalKeyboardHook();
+
+
+       string dosyayolu,eskidosyayolu;
+        int dosyasayisi ;
+        
+       
+
+        public Form1()
+        {
+            //  this.TopMost = true;          
+            InitializeComponent();
+         //   this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+
+
+
+           //this.Load += new EventArgs(Form1_Load);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        { 
+            
+           
+                           
+           if(e.KeyCode ==Keys.E)
+            {
+                button1.PerformClick();
+            }
+            }
+
+        void gkh_KeyDown(object sender, KeyEventArgs e)
+        {
+            try { 
+          if(e.KeyCode == Keys.Space)
+            {
+                button1.PerformClick();
+                e.Handled = true;
+            }
+            }
+            catch(Exception)
+            {
+
+            }
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gkh.HookedKeys.Add(Keys.Space);
+            gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Keydown(object sender, KeyEventArgs e)
+        {
+          
+            
+            
+        }
+
+        private void pictureBox1_Keydown(object sender,KeyEventArgs e)
+        {
+
+        }
+        
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+
+                /*    int screenLeft = SystemInformation.VirtualScreen.Left;
+              int screenTop = SystemInformation.VirtualScreen.Top;
+              int screenWidth = SystemInformation.VirtualScreen.Width;
+              int screenHeight = SystemInformation.VirtualScreen.Height;
+              eskidosyasayisi = dosyasayisi;
+
+
+                  using (Bitmap bmp = new Bitmap(screenWidth, screenHeight))
+                  {
+                      using (Graphics g = Graphics.FromImage(bmp))
+                      {
+                          String filename = "RakipEkran-" + DateTime.Now.ToString("ddMMyyyy-hhmmss") + ".png";
+                          Opacity = .0;
+                          g.CopyFromScreen(screenLeft, screenTop, 0, 0, bmp.Size);
+                          bmp.Save("Z:\\ScreenShoots\\" + filename);
+                          Opacity = 1;
+
+                          pictureBox1.Image = Image.FromFile("Z:\\ScreenShoots\\" + filename);
+                      }
+                  }*/
+
+
+                //////////////////////
+                this.Hide();
+                System.Threading.Thread.Sleep(100);
+                 SendKeys.Send("{PRTSC}");
+                    Image myImage = Clipboard.GetImage();
+                    dosyayolu = "C:\\tftss\\board"+ dosyasayisi +".jpg";
+                    pictureBox1.Image = myImage;
+                 eskidosyayolu = dosyayolu;
+                    File.Delete(eskidosyayolu);
+                 // dosyasayisi =  dosyasayisi + 1;
+                    myImage.Save(dosyayolu);
+                    this.Show();
+                     
+
+            }
+            catch(Exception)
+            {
+
+            }
+            
+        }
+    }
+}
